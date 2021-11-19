674f245cf52e2e4ac2ce90c8056bec325ceeb3ff
FesalBadday <fesal.n04@hotmail.com>
2021-11-19 13:39:21 -0700

    added nav

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="post in posts" :key="post.slug">
-        <NuxtLink v-bind:to="post.slug">{{ post.title }}</NuxtLink>
-      </li>
-    </ul>
-  </div>
-</template>
+  <section>
+    <nav>
+      <h2>Articles</h2>
+      <ul>
+        <li v-for="post in posts" :key="post.slug">
+          <NuxtLink :to="`blog/${post.slug}`">{{ post.title }}</NuxtLink>
+        </li>
+      </ul>
 
+      <h2>Gallery</h2>
+      <ul>
+        <li v-for="gallery in galleries" :key="gallery.slug">
+          <NuxtLink :to="`gallery/${gallery.slug}`">{{ gallery.title }}</NuxtLink>
+        </li>
+      </ul>
+
+      <h2>Testimonial</h2>
+      <ul>
+        <li v-for="testimonial in testimonials" :key="testimonial.slug">
+          <NuxtLink :to="`testimonials/${testimonial.slug}`">{{ testimonial.quote }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </section>
+</template>
 <script>
 export default {
+  // fetch the data from the blog in the content folder
   async asyncData({ $content }) {
     const posts = await $content("blog").fetch();
+    const galleries = await $content("gallery").fetch();
+    const testimonials = await $content("testimonials").fetch();
     return {
       posts,
+      galleries,
+      testimonials,
     };
   },
 };
